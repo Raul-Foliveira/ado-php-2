e9b7a34fcb9fe10a43edf4eab88ad4570de33f1e
Uthiele Guimaraes <uthieli21@gmail.com>
2023-05-28 23:25:36 -0300

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -1,20 +1,11 @@
 <?php
-function conexao() {
-    // configurações de conexão
-    $servidor = "localhost";
-    $usuario = "seu_usuario";
-    $senha = "sua_senha";
-    $banco = "seu_banco";
+ //    DataBase info.
+ define('HOST', 'localhost');
+ define('USUARIO', 'root');
+ define('SENHA', '');
+ define('DB', 'dbfamarcia');
 
-    // conexão com o banco de dados
-    $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
-
-    // verifica se houve erro na conexão
-    if (!$conexao) {
-        die("Conexão falhou: " . mysqli_connect_error());
-    }
-
-    // retorna a conexão
-    return $conexao;
-}
+ $pdoClient = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possivel conectar!');
+ mysqli_select_db($pdoClient, DB);
+ mysqli_set_charset($pdoClient,"utf8");
 ?>
