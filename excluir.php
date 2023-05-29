583a11104bc086a6e66efd3ffb36ac596e82cdaa
Uthiele Guimaraes <uthieli21@gmail.com>
2023-05-28 23:27:33 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,10 +1,32 @@
 <?php
+    require_once 'conectar.php';
+
+try {
 include "abrir_transacao.php";
 include_once "operacoes.php";
+
+function excluirDados($chave) {
+    global $pdoClient;
+   
+    
+    $query = "DELETE FROM medicamento WHERE chave=$chave";
+    $result = mysqli_query($pdoClient, $query);
+    
+   
+
+}
 
 $chave = (int) $_POST["chave"];
 $id = excluirDados($chave);
 
+
 header("Location: listar.php");
+
+$transacaoOk = true;
+
+} finally {
+
+
 include "fechar_transacao.php";
+}
 ?>
