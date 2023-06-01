f690e2d43b560d6850103a996f9ccf52d14ed43c
Raulzera <117924299+Raul-Foliveira@users.noreply.github.com>
2023-05-31 22:52:36 -0300

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -1,12 +1,11 @@
 <?php
- //    DataBase info.
- define('HOST', 'localhost');
- define('USUARIO', 'root');
- define('SENHA', '');
- define('DB', 'dbfamarcia');
-
- $pdoClient = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possivel conectar!');
- mysqli_select_db($pdoClient, DB);
- mysqli_set_charset($pdoClient,"utf8");
+function conectar () {
+    try {
+        $db_file = "C:\\xampp\\htdocs\\db.sqlite";
+        return new PDO("sqlite:$db_file");
+    } catch (PDOException $e) {
+        echo "Erro: " . $e->getMessage();
+        throw $e;
+    }
+}
 ?>
-
