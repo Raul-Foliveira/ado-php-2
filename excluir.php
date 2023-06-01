4359ab4e06568ba28802b11030251beb15c019a3
Raulzera <117924299+Raul-Foliveira@users.noreply.github.com>
2023-05-31 22:53:00 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,17 +1,20 @@
 <?php
-require_once 'conectar.php';
+try {
+    include "abrir_transacao.php";
+    include_once "operacoes.php";
 
-    function excluirDados($chave, $conexao) {
-        $chave = mysqli_real_escape_string($conexao, $chave);
-        $query = "DELETE FROM medicamento WHERE chave=$chave";
-        $result = mysqli_query($conexao, $query);
-        return $result;
-    }
+ 
 
-    $chave = isset($_GET["chave"]) ? (int) $_GET["chave"] : 0;
-    $id = excluirDados($chave, $pdoClient);
+    $chave = (int) $_POST["chave"];
+    $id = excluirDados($chave);
 
     header("Location: listar.php");
-    exit();
 
+
+    $transacaoOk = true;
+
+
+} finally {
+    include "fechar_transacao.php";
+}
 ?>
